db3079d2e4f5dd49cdc651166ca74debf4037198
joyoungc <aiden.jeong.jy@gmail.com>
2020-07-16 11:29:48 +0900

    Initialize the project structure using Gradle

---
+++ app-data/src/main/java/io/joyoungc/app/data/AppDataApplication.java
@@ -0,0 +1,15 @@
+package io.joyoungc.app.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * Created by Aiden Jeong on 2020.07.15
+ */
+@SpringBootApplication
+public class AppDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppDataApplication.class, args);
+    }
+
+}
